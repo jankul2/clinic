60d0021ac7a81c2237fdfb0417f4ea9133519db1
ankulkumar1989it@gmail.com <ankulkumar1989it@gmail.com>
2022-04-11 13:58:01 +0530

    uploaded some code with new functionlaty auth, helper,functions

--- dbconfig/connection.js
+++ dbconfig/connection.js
@@ -3,21 +3,21 @@ import createError from 'http-errors';
 import path from 'path';
 import mysql from 'mysql2';
 class Connection {
-    constructor(){
-    this.conn = mysql.createPool({
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'clinic',
-        waitForConnections: true,
-        connectionLimit: 10,
-        queueLimit: 0
-    });
-    
-}
+    constructor() {
+        this.conn = mysql.createPool({
+            host: process.env.host,
+            user: process.env.user,
+            password: process.env.password,
+            database: process.env.database,
+            waitForConnections: true,
+            connectionLimit: 10,
+            queueLimit: 0
+        });
+
+    }
 
 }
 
-const dbconnection=new Connection();
+const dbconnection = new Connection();
 
 export default Connection;
