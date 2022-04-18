58f772a447ed7f83838a57d85ab8efc69868278b
ankulp <ankulkumar1989it@gmail.com>
2022-04-18 22:22:25 +0530

    my personal backup

---
+++ dbconfig/connection.js
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+import createError from 'http-errors';
+import path from 'path';
+import mysql from 'mysql2';
+dotenv.config();
+class Connection {
+    constructor() {
+        this.conn = mysql.createPool({
+            host: process.env.host,
+            user: process.env.user,
+            password: process.env.password,
+            database: process.env.database,
+            waitForConnections: true,
+            connectionLimit: 10,
+            queueLimit: 0
+        });
+
+    }
+
+}
+
+const dbconnection = new Connection();
+
+export default Connection;
