f5e91db70983358b256c23ed7e2004b23a88332a
ankulp <ankulkumar1989it@gmail.com>
2022-04-23 16:43:10 +0530

    pdf code and email template code

--- dbconfig/connection.js
+++ dbconfig/connection.js
@@ -5,6 +5,7 @@ import mysql from 'mysql2';
 dotenv.config();
 class Connection {
     constructor() {
+        console.log(process.env.host,process.env.password,process.env.user,process.env.database);
         this.conn = mysql.createPool({
             host: process.env.host,
             user: process.env.user,
